269990b413d2169227ae5d0d81fc2995cf17be4b
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-04 22:54:04 +0500

    Register persistent fantasy draft service

--- apps/api/src/modules/fantasy/fantasy.module.ts
+++ apps/api/src/modules/fantasy/fantasy.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { SportmonksModule } from '../sportmonks/sportmonks.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { FantasyTeamService } from './fantasy-team.service';
+import { FantasyDraftService } from './fantasy-draft.service';
 import { ContestService } from './contest.service';
 import { FantasyTeamController, ContestController } from './fantasy.controller';
 import { FirestoreService } from '../../common/firestore.service';
 
 @Module({
   imports: [SportmonksModule, WalletModule],
-  providers: [FantasyTeamService, ContestService, FirestoreService],
+  providers: [FantasyTeamService, FantasyDraftService, ContestService, FirestoreService],
   controllers: [FantasyTeamController, ContestController],
-  exports: [FantasyTeamService, ContestService],
+  exports: [FantasyTeamService, FantasyDraftService, ContestService],
 })
 export class FantasyModule {}
