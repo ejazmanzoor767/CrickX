c711afa3e86e7fd50c4c80753a50c478ed5b0806
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-05 00:02:18 +0500

    Register leaderboard service and controller

--- apps/api/src/modules/scoring/scoring.module.ts
+++ apps/api/src/modules/scoring/scoring.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { SportmonksModule } from '../sportmonks/sportmonks.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { ScoringService } from './scoring.service';
+import { LeaderboardService } from './leaderboard.service';
+import { LeaderboardController } from './leaderboard.controller';
 import { FirestoreService } from '../../common/firestore.service';
 
 @Module({
   imports: [SportmonksModule, WalletModule],
-  providers: [ScoringService, FirestoreService],
-  exports: [ScoringService],
+  providers: [ScoringService, LeaderboardService, FirestoreService],
+  controllers: [LeaderboardController],
+  exports: [ScoringService, LeaderboardService],
 })
 export class ScoringModule {}
