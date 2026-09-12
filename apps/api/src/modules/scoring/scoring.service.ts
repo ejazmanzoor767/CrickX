2a47f2d1a8e5e25db5b0b5f6d7b1069ef683e5b9
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-12 13:25:41 +0500

    Fix scoring service TypeScript build after pool ID migration

--- apps/api/src/modules/scoring/scoring.service.ts
+++ apps/api/src/modules/scoring/scoring.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { FirestoreService } from '../../common/firestore.service';
 import { SportmonksDataService } from '../sportmonks/sportmonks-data.service';
