7330c5cf43a897cb02a78ca0c97487dfdbf6321e
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-22 23:04:23 +0500

    Force single React instance in mobile Metro

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// This monorepo contains React 18.2.0 for the Expo app and React 18.3.1
+// for the web app. Force Metro to use one React instance in the native app
+// so React hooks share the same dispatcher.
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    ...(config.resolver?.extraNodeModules || {}),
+    react: path.resolve(__dirname, 'node_modules/react'),
+  },
+};
+
+module.exports = config;
