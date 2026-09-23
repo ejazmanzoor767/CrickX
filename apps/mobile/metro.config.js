d4166b9cde7eaebba4f860e0b3f0f8d68aac5895
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-23 11:43:52 +0500

    Point mobile Metro to shared React

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,14 +3,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// This monorepo contains React 18.2.0 for the Expo app and React 18.3.1
-// for the web app. Force Metro to use one React instance in the native app
+// This monorepo contains React 18.3.1 is shared by the Expo app and web app. Force Metro to use one React instance in the native app
 // so React hooks share the same dispatcher.
 config.resolver = {
   ...config.resolver,
   extraNodeModules: {
     ...(config.resolver?.extraNodeModules || {}),
-    react: path.resolve(__dirname, 'node_modules/react'),
+    react: path.resolve(__dirname, '../../node_modules/react'),
   },
 };
 
