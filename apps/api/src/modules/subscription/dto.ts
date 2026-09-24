f836a5decebe6ec8b9332ccec9f48e07e7699f27
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-24 22:08:44 +0500

    Validate referral code input

--- apps/api/src/modules/subscription/dto.ts
+++ apps/api/src/modules/subscription/dto.ts
@@ -1,1 +1,7 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateSubscriptionCheckoutDto {}
+
+export class ApplyReferralDto {
+  @IsString() @MinLength(4) @MaxLength(32) code!: string;
+}
