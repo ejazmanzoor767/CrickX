da814bb9d93543b5324187dc9bda95ba921fca59
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-24 22:04:15 +0500

    Replace vulnerable native bcrypt dependency with bcryptjs

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import * as crypto from 'crypto';
 import { FirestoreService } from '../../common/firestore.service';
 import { RegisterDto, LoginDto } from './dto';
