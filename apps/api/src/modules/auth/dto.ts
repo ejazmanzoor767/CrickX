bbd5f1f537627126b880e8a753ea06f7dbc3b953
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-24 21:53:14 +0500

    Harden authentication DTO limits

--- apps/api/src/modules/auth/dto.ts
+++ apps/api/src/modules/auth/dto.ts
@@ -1,17 +1,17 @@
-import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsOptional, MaxLength } from 'class-validator';
 
 export class RegisterDto {
-  @IsEmail() email!: string;
-  @IsString() @MinLength(8) password!: string;
-  @IsString() displayName!: string;
-  @IsOptional() @IsString() phone?: string;
+  @IsEmail() @MaxLength(254) email!: string;
+  @IsString() @MinLength(8) @MaxLength(128) password!: string;
+  @IsString() @MinLength(1) @MaxLength(80) displayName!: string;
+  @IsOptional() @IsString() @MaxLength(32) phone?: string;
 }
 
 export class LoginDto {
-  @IsEmail() email!: string;
-  @IsString() password!: string;
+  @IsEmail() @MaxLength(254) email!: string;
+  @IsString() @MaxLength(128) password!: string;
 }
 
 export class RefreshDto {
-  @IsString() refreshToken!: string;
+  @IsString() @MinLength(32) @MaxLength(256) refreshToken!: string;
 }
