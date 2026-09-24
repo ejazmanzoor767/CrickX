1b66e7512a7ec315e9665f1d854019a41cf1fa68
ejazmanzoor767 <ejazmanzoor767@gmail.com>
2026-09-24 13:11:55 +0500

    Wire OxaPay into wallet token purchases

--- apps/api/src/modules/wallet/wallet.module.ts
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -5,10 +5,13 @@ import { RazorpayWebhookController } from './webhook.controller';
 import { RazorpayService } from './razorpay.service';
 import { PayoutService } from './payout.service';
 import { FirestoreService } from '../../common/firestore.service';
+import { SubscriptionModule } from '../subscription/subscription.module';
+import { OxaPayWalletWebhookController } from './webhook.controller';
 
 @Module({
+  imports: [SubscriptionModule],
   providers: [WalletService, RazorpayService, PayoutService, FirestoreService],
-  controllers: [WalletController, RazorpayWebhookController],
+  controllers: [WalletController, RazorpayWebhookController, OxaPayWalletWebhookController],
   exports: [WalletService, PayoutService],
 })
 export class WalletModule {}
